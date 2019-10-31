37f05426418ed755e896a99e39ff0e95fdd53563
RenMIYANOHARA-private <miyah855_japan@outlook.jp>
2019-11-01 02:25:32 +0900

    First message

--- client.py
+++ client.py
@@ -1,3 +1,36 @@
+"""Attention
+    Unable to use character except ASCII
+"""
 
+import socket
+import chardet
 
-print('Hello server !')
+class Client:
+
+    def __init__(self, message):
+
+        self.message = message
+
+    def encode(self, code):
+
+        if code == 'utf-8':
+            self.code = code
+            self.message_byte = self.message.encode('utf-8')
+
+    def client(self):
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            # サーバを指定
+            s.connect(('127.0.0.1', 50007))
+            # サーバにメッセージを送る
+            s.sendall(b'' + self.message_byte)
+            # ネットワークのバッファサイズは1024。サーバからの文字列を取得する
+            data = s.recv(1024)
+            #
+            print(repr(data.decode('utf-8')))
+
+if __name__ == '__main__':
+
+    client = Client('いろはにほへと')
+    client.encode('utf-8')
+    client.client()
--- server.py
+++ server.py
@@ -1,3 +1,37 @@
+import socket
 
+class Server:
 
-print('Hello client !')
+    def __init__(self):
+
+        self.dict = {'山': '川',
+                      '努力': '根性',
+                      'いろはにほへと': 'うぬのおくやま',
+                      '銀河': '多摩川'}
+
+    def server(self):
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            # IPアドレスとポートを指定
+            s.bind(('127.0.0.1', 50007))
+            # 1 接続
+            s.listen(1)
+            # connection するまで待つ
+            while True:
+
+                print('Now listening... ')
+                # 誰かがアクセスしてきたら、コネクションとアドレスを入れる
+                conn, addr = s.accept()
+                with conn:
+                    while True:
+                        # データを受け取る
+                        data = conn.recv(1024)
+                        if not data:
+                            break
+                        # クライアントにデータを返す(b -> byte でないといけない)
+                        conn.sendall(b'' + self.dict[data.decode('utf-8')].encode('utf-8'))
+
+if __name__ == '__main__':
+
+    server = Server()
+    server.server()
